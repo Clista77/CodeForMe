759054d99d29ac582cbe75b3e02b19ed5b7923fb
Clista77 <59404191+Clista77@users.noreply.github.com>
2020-01-07 16:54:42 +0900

    Delete Anova.R

--- Anova.R
+++
@@ -1,9 +0,0 @@
-data(iris)
-names(iris)=c('sl','sw','pl','pw','sp')
-anova(aov(sl~sp,data=iris))
-anova(aov(sw~sp,data=iris))
-anova(aov(pl~sp,data=iris))
-anova(aov(pw~sp,data=iris))
-
-TukeyHSD(aov(sl~sp,data=iris))
-plot(TukeyHSD(aov(sl~sp,data=iris)))
